9e8e62cb98a6d6cb53d1dc6cf9da0c4fe50ae6ee
Lucas Cruz <lucas.cruz@modalgr.io>
2024-10-31 18:23:23 -0300

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Contact from "./components/Contact"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Tech from "./components/Tech"
+
 function App() {
   return (
     <>
-      <div className="text-8xl font-bold">hello world</div>
+      <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#344f91_100%)]"></div>
+      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+        <Navbar />
+        <Hero />
+        <Tech />
+        <Projects />
+        <Contact />
+      </main>
     </>
   )
 }
